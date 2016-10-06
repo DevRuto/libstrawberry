2e32f2282dfe5cae4cfc65f0826e686da16eba99
God <strawberryentrypoint@gmail.com>
2016-10-06 21:19:56 +0100

    Refactoring.

--- libstrawberry/crypto/hashing/sha224.c
+++ libstrawberry/crypto/hashing/sha224.c
@@ -34,6 +34,8 @@
 
 #if !defined(SB_EXCLUDE_CRYPTO_HASHING) && !defined(SB_EXCLUDE_CRYPTO_HASHING_SHA224)
 
+#define SB_INTRINSICS
+
 #include "./sha224.h"
 
 #include "../../core/error.h"
@@ -217,27 +219,32 @@ sb_bool_t sb_crypto_sha224(uint8_t out[28], void *in, sb_size_t size) {
 	sb_crypto_sha224_ctx_t ctx;
 	sb_crypto_sha224_init(&ctx);
 
-	sb_size_t size_rounded = sb_math_round_block(64, (size + 1));
+	sb_size_t buffer_size = sb_math_round_block(64, (size + 1));
 
-	uint8_t output[size_rounded];
-	sb_memcpy(output, in, size);
-	sb_memset((output + size), 0, size_rounded - size);
+	SB_MEM_BUFFER_ALLOC(uint8_t, buffer, buffer_size);
 
-	output[size] = 0x80;
-	output[size_rounded - 1] =  (8 * size);
-	output[size_rounded - 2] = ((8 * size) >>  8);
-	output[size_rounded - 3] = ((8 * size) >> 16);
-	output[size_rounded - 4] = ((8 * size) >> 24);
+	sb_memcpy(buffer, in, size);
+	sb_memset((buffer + size), 0, buffer_size - size);
 
-	uint32_t block[16], *in32 = (uint32_t*)output;
+	buffer[size] = 0x80;
+
+	buffer[buffer_size - 1] = ( (8 * size)        & 0xFF);
+	buffer[buffer_size - 2] = (((8 * size) >>  8) & 0xFF);
+	buffer[buffer_size - 3] = (((8 * size) >> 16) & 0xFF);
+	buffer[buffer_size - 4] = (((8 * size) >> 24) & 0xFF);
+
+	uint32_t block[16], *in32 = (uint32_t*)buffer;
+
 	sb_size_t i, j;
-	for (i = (size_rounded / 64); i--;) {
+	for (i = (buffer_size / 64); i--;) {
 		for (j = 0; j < 16; ++j) {
 			block[j] = SB_BE32(*in32);
 			++in32;
 		}
 		sb_crypto_sha224_update(&ctx, block);
 	}
+
+	SB_MEM_BUFFER_FREE(buffer);
 
 	sb_bool_t valid = sb_crypto_sha224_finish(&ctx, out);
 
